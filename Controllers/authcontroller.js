b724e72e97145312dde1355692841cfea04dd7ac
Shivanshu garg <gargshivanshu000@gmail.com>
2026-07-02 17:45:48 +0530

    All Error fixed Gemini ai Changed to Groq ai

--- Controllers/authcontroller.js
+++ Controllers/authcontroller.js
@@ -29,7 +29,7 @@ const token = generateToken({
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 24 * 60 *  60 * 1000,
     });
@@ -75,7 +75,7 @@ const token = generateToken({
     return res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
