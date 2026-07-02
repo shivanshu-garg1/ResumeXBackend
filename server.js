9a3ec0eed964cf484f5be0a8ecd77efcbaf15eeb
Shivanshu garg <gargshivanshu000@gmail.com>
2026-07-02 17:34:14 +0530

    All Error fixed Gemini ai Changed to Groq ai

--- server.js
+++ server.js
@@ -16,13 +16,18 @@ app.use(cookieParser());
 
 app.use(
   cors({
-  origin: [
-    "http://localhost:8080",
-    "https://resumex-flax.vercel.app/"
-  ],
+    origin: [
+      "https://resumex-flax.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use(express.json());
+
 const authRoutes = require("./Routes/authRoutes");
 const atsRoutes = require("./Routes/atsRoutes");
 
